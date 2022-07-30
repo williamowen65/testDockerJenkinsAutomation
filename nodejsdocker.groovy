57c4c9846b3e809976c4d19ddbfc21c838c3bd79
William Owen <66016649+williamowen65@users.noreply.github.com>
2022-07-30 14:57:47 -0700

    Update nodejsdocker.groovy

--- nodejsdocker.groovy
+++ nodejsdocker.groovy
@@ -1,27 +1,14 @@
-job('NodeJS Docker example') {
-    scm {
-        git('https://github.com/williamowen65/frontend.git') {  node -> // is hudson.plugins.git.GitSCM
-            node / gitConfigName('DSL User')
-            node / gitConfigEmail('jenkins-dsl@newtech.academy')
-        }
-    }
-    triggers {
-        scm('H/5 * * * *')
-    }
-    wrappers {
-        nodejs('nodejs') // this is the name of the NodeJS installation in 
-                         // Manage Jenkins -> Configure Tools -> NodeJS Installations -> Name
-    }
-    steps {
-        input 'hello is this right?'
-        dockerBuildAndPublish {
-            repositoryName('islandhopper152/docker-nodejs-demo')
-            tag('${GIT_REVISION,length=9}')
-            registryCredentials('dockerhub')
-            forcePull(false)
-            forceTag(false)
-            createFingerprints(false)
-            skipDecorate()
+pipelineJob('github-demo') {
+    definition {
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        github('jenkinsci/pipeline-examples')
+                    }
+                }
+            }
+            scriptPath('declarative-examples/simple-examples/environmentInStage.groovy')
         }
     }
 }
