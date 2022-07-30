0e14caa28d786c8fb9ee652a06d1a55a71d0ec0d
William Owen <66016649+williamowen65@users.noreply.github.com>
2022-07-30 15:19:27 -0700

    Update nodejsdocker.groovy

--- nodejsdocker.groovy
+++ nodejsdocker.groovy
@@ -8,7 +8,7 @@ pipelineJob('read-voices-test') {
                     }
                 }
             }
-            scriptPath('Jenkinsfile')
+            scriptPath('./Jenkinsfile')
         }
     }
     triggers {
